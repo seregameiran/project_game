0767229264880acea7978501edb53e2d55ecebb4
seregameiran <sergeymajran@gmail.com>
2026-03-12 01:42:03 +1000

    creation of architecture

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(){
+    RenderWindow window (VideoMode(800, 608), " ");
+
+    while (window.isOpen()){
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
